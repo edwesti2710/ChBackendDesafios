2500ddc4aab6a340890e63be3635d049a564a1aa
Edward Espinoza <edwesti2710@gmail.com>
2023-02-14 17:36:01 -0500

    first commit

---
+++ Desafio1/index.js
@@ -0,0 +1,46 @@
+class ProductManager {
+    constructor() {
+        this.products = [];
+    }
+    addProduct(newProduct) {
+
+        if (!newProduct.title || !newProduct.description || !newProduct.price || !newProduct.thumbnail || !newProduct.code || !newProduct.stock) {
+            return console.log('All fields are required!');
+        } else {
+            const products = this.getProducts();
+            if (products.some(product => product.code === newProduct.code)) {
+                return console.log('This product already exists!')
+            } else {
+                newProduct.id = products.length + 1;
+                this.products.push(newProduct);
+                console.log('Product was successfully added')
+                return newProduct.id;
+            }
+        }
+    }
+    getProducts() {
+        return this.products;
+    }
+    getProductById(id) {
+        const products = this.getProducts();
+        let desiredProduct = products.find(product => product.id === id)
+        return desiredProduct ? console.log(desiredProduct) : console.log('Not Found');
+    }
+}
+
+// const productsManager = new ProductManager();
+
+// console.log(productsManager.getProducts())
+
+// productsManager.addProduct({
+//     title: 'producto prueba',
+//     description: 'Este es un producto prueba',
+//     price: 200,
+//     thumbnail: 'Sin imagen',
+//     code: 'abc123',
+//     stock: 25
+// });
+
+// console.log(productsManager.getProducts());
+
+// productsManager.getProductById(2);
